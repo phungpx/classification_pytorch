9e8a5208b5db371615e66d25e29ec2a8ab390d5c
phungpx <phamxuanphungbku@gmail.com>
2021-11-16 11:52:27 +0700

    Add mean, std for dataloader

--- flame/core/data/text_rotation.py
+++ flame/core/data/text_rotation.py
@@ -17,6 +17,8 @@ class TextRotationDataset(Dataset):
         image_height: int = 32,  # fix heigh of text line image, width = image_ratio * image_height
         image_ratio: int = 20,  # ensure ratio of image (w / h)
         inner_size: int = 48,  # fixed dimension for min dimension of image
+        mean: Optional[Tuple[float, float, float]] = None,
+        std: Optional[Tuple[float, float, float]] = None,
         transforms: Optional[List[Callable]] = None,
         max_transform: int = 5,
     ):
@@ -26,6 +28,9 @@ class TextRotationDataset(Dataset):
         self.image_height = image_height
         self.transforms = transforms if transforms else []
         self.max_transform = min(max_transform, len(self.transforms))
+
+        self.mean = np.array(mean, dtype=np.float).reshape(1, 1, 3) if mean is not None else None
+        self.std = np.array(std, dtype=np.float).reshape(1, 1, 3) if std is not None else None
 
         image_paths = []
         [image_paths.extend(Path(datadir).glob(image_pattern)) for datadir in datadirs]
@@ -49,8 +54,11 @@ class TextRotationDataset(Dataset):
 
     def __getitem__(self, idx):
         image_path, target = self.image_paths[idx]
-
         image = cv2.imread(str(image_path))
+
+        if (self.mean is not None) and (self.std is not None):
+            image = cv2.cvtColor(image, cv2.COLOR_BGR2RGB)
+
         image = self.resize(image)  # narrow down image size
 
         for transform in random.sample(self.transforms, k=random.randint(0, self.max_transform)):
@@ -62,10 +70,16 @@ class TextRotationDataset(Dataset):
         # crop appropriate ratio and then normalize image to mean=0 and std=1
         if sample.shape[1] / sample.shape[0] > self.image_ratio:
             sample = self.crop_to_ratio(image=sample)
-            sample = (sample - sample.mean()) / sample.std() if sample.std() else np.zeros_like(sample)
+            if (self.mean is not None) and (self.std is not None):
+                sample = (sample / 255 - self.mean) / self.std
+            else:
+                sample = (sample - sample.mean()) / sample.std() if sample.std() else np.zeros_like(sample)
         # normalize image to mean=0, std=1 and then pad to appropriate ratio
         else:
-            sample = (sample - sample.mean()) / sample.std() if sample.std() else np.zeros_like(sample)
+            if (self.mean is not None) and (self.std is not None):
+                sample = (sample / 255 - self.mean) / self.std
+            else:
+                sample = (sample - sample.mean()) / sample.std() if sample.std() else np.zeros_like(sample)
             sample = self.pad_to_ratio(image=sample)
 
         # resize to fixed size and then convert to tensor (C x H x W)
